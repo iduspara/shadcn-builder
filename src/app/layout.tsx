5199d7ce16cdef265615f02353f344174e4e66e5
Igor Duspara <igor.duspara@finanztip.de>
2025-03-30 06:33:44 +0200

    Added PostHog package

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,19 @@
 import "./globals.css";
 import { cn } from "@/lib/utils";
 import { fontVariables } from "@/lib/fonts";
-import { Analytics } from "@vercel/analytics/react"
+import { Analytics } from "@vercel/analytics/react";
+import { PostHogProvider } from "@/components/PostHogProvider";
 
 export default function RootLayout({
   children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+}: Readonly<{ children: React.ReactNode }>) {
   return (
     <html lang="en">
       <body className={cn(fontVariables)}>
-        {children}
-        <Analytics />
+        <PostHogProvider>
+          {children}
+          <Analytics />
+        </PostHogProvider>
       </body>
     </html>
   );
